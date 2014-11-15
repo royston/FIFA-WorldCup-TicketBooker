fbea7a4ba88a859885584a4eb986ed23fe958ba8
Royston Monteiro <roystonpm@gmail.com>
2014-11-15 13:47:55 -0500

    Update automator-worker.js

--- automator-worker.js
+++ automator-worker.js
@@ -1,10 +1,15 @@
 var i = 1;   
+
+//Delay between polls
 var delay = 3;
+//A sufficiently large number. The number of times the script wiill poll
 var polls = 999;
+//variable that specifies which match numbers to watch out for.(match 64 is the final)
 var matches = '44|61|64';
 function myLoop () {           
    setTimeout(function () {    
       $.ajax({
+         //The XHR request that the FIFA website uses to refresh results.
   url: "https://fwctickets.fifa.com/TopsAkaCalls/Calls.aspx/getRefreshChartAvaDem?l=en&c=OTH",
   context: document.body
 }).done(function(data) {
